3a2b9a905aa7963010b35be42f9664fba2445053
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2006-12-20 15:42:57 +0000

    add simple script to build deps

---
+++ trunk/deps/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# JPEG
+if test ! -f /usr/local/lib/libjpeg.a ; then
+    cd jpeg-6b
+    sh configure
+    make
+    sudo mkdir /usr/local/lib /usr/local/include
+    sudo cp libjpeg.a /usr/local/lib
+    sudo cp *.h /usr/local/include
+    cd -
+fi
+
+# PIL
+cd Imaging-1.1.6
+sudo python setup.py install
+cd -
