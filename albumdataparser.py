9d52d6d73b30c1569a341bd13341bec12d00b2f0
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2006-12-20 02:27:44 +0000

    Initial import

---
+++ albumdataparser.py
@@ -0,0 +1,129 @@
+from os.path import expanduser
+from xml.parsers.expat import ParserCreate, ExpatError
+from photo import Photo
+
+
+class AlbumData(object):
+    def __init__(self, data):
+        self.data = data
+
+    def getAlbumByName(self, name):
+        for album in self.data['List of Albums']:
+            if album['AlbumName'] == name:
+                return album
+        raise Error, "Album \"%s\" not in iPhoto Library" % name
+
+    def getPicturePathFromId(self, id):
+        return self.data['Master Image List'][id]['ImagePath']
+
+    def getPicturesIdFromAlbumName(self, name):
+        res = []
+        album = self.getAlbumByName(name)
+        return album['KeyList']
+
+    def getPhotoFromId(self, id):
+        p = self.data['Master Image List'][id]
+        return Photo(p['ImagePath'],
+                     id,
+                     p['Caption'],
+                     p['Comment'],
+                     p['DateAsTimerInterval'])
+
+    def getPhotosFromAlbumName(self, name):
+        res = []
+        pics = self.getPicturesIdFromAlbumName(name)
+        for pic in pics:
+            res.append(self.getPhotoFromId(pic))
+        return res
+
+
+class XmlItem(object):
+    def __init__(self, type):
+        self.type = type
+        self.value = None
+
+    def setValue(self, val):
+        if self.type == 'key':
+            self.value = val
+        elif self.type == 'string':
+            self.value = val.encode('utf8')
+        elif self.type == 'integer':
+            self.value = int(val)
+        elif self.type == 'real':
+            self.value = float(val)
+        elif self.type == 'true':
+            self.value = True
+        elif self.type == 'false':
+            self.value = False
+        else:
+            raise ExpatError, "Type \"%s\" not supported" % type
+
+class AlbumDataParser(object):
+    def __init__(self, xmlFileName=expanduser('~/Pictures/iPhoto Library/AlbumData.xml')):
+        self.xmlFile = file(xmlFileName, 'r')
+        self.elemList = []
+        self.lastItemData = ''
+        self.keys = []
+        self.albumData = None
+
+    def addItem(self, item):
+            list = self.elemList[-1]
+            if str(list.__class__) == "<type 'dict'>":
+                list[self.keys.pop()] = item
+            elif str(list.__class__) == "<type 'list'>":
+                list.append(item)
+            else:
+                raise ExpatError, list.__class__
+
+    def parse(self):
+        def start_element(name, attrs):
+            if name == 'dict':
+                elem = {}
+            elif name in ['plist', 'array']:
+                elem = []
+            elif name in ['key', 'string', 'integer', 'real', 'true', 'false']:
+                elem = XmlItem(name)
+            else:
+                raise ExpatError, "Element \"%s\" not supported" % name
+            self.elemList.append(elem)
+
+        def end_element(name):
+            item = self.elemList.pop()
+            if name == 'plist':
+                self.albumData = AlbumData(item[0])
+                assert(len(self.elemList) == 0)
+            elif name == 'dict':
+                self.addItem(item)
+            elif name == 'array':
+                self.addItem(item)
+            elif name in ['key', 'string', 'integer', 'real', 'true', 'false']:
+                item.setValue(self.lastItemData)
+                if name == 'key':
+                    self.keys.append(item.value)
+                else:
+                    self.addItem(item.value)
+            else:
+                raise ExpatError, "Element \"%s\" not supported" % name
+            self.lastItemData = ''
+
+        def char_data(data):
+            if not data.startswith('\n') and not data.startswith('\t'):
+                self.lastItemData += data
+
+        p = ParserCreate()
+        p.StartElementHandler = start_element
+        p.EndElementHandler = end_element
+        p.CharacterDataHandler = char_data
+        p.ParseFile(self.xmlFile)
+        return self.albumData
+
+
+if __name__ == '__main__':
+    adp = AlbumDataParser()
+    data = adp.parse()
+    alb = data.getAlbumByName("Test")
+    print alb['Album Type']
+    print alb['KeyList']
+    print
+    for f in data.getPictureFilesFromAlbumName('Test'):
+        print f
---
+++ makepage.py
@@ -0,0 +1,93 @@
+from albumdataparser import AlbumDataParser
+import os, sys
+
+
+class WebPage(object):
+    def __init__(self, fileName, title):
+        self.fileName = fileName  + ".html"
+        self.title = title
+        self.code = ''
+
+    def addCode(self, s):
+        self.code += s
+
+    def addCodeLine(self, s):
+        self.code += s + '\n'
+
+    def getHeader(self):
+        return '''
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 TRANSITIONAL//EN">
+    <html>
+    <head>
+    <title>%s</title>
+    <link href="../style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>''' % self.title
+
+    def getFooter(self):
+        return '''
+    </body>
+    </html>
+    '''
+
+    def writePage(self):
+        out = file(self.fileName, 'w')
+        out.write(self.getHeader())
+        out.write(self.code)
+        out.write(self.getFooter())
+        out.close()
+
+def echo(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+def makePhotoPage(photo, linkBack):
+    page = WebPage(photo.id, photo.title)
+    page.addCodeLine('<div class="square"><a href="%s"><img class="prev" src="%s" /></a></div>'
+        % (linkBack, photo.prevPath))
+    page.writePage()
+    return page.fileName
+
+echo("Parsing AlbumData.xml")
+albumName = sys.argv[1]
+parser = AlbumDataParser()
+data = parser.parse()
+photos = data.getPicturesIdFromAlbumName(albumName)
+echo("\t[DONE]\n")
+
+outputPath = 'out'
+dirs = ['photos', 'preview', 'thumbs']
+os.chdir(outputPath)
+if not os.path.exists(albumName):
+	os.makedirs(albumName)
+os.chdir(albumName)
+for dir in dirs:
+	if not os.path.exists(dir): 
+		os.mkdir(dir)
+
+picsPerPage = 6 ** 2
+pageCounter = 1
+curPage = WebPage("page%2d" % pageCounter, albumName)
+curPage.addCode("<div class=\"square\">")
+
+echo("Writing pictures 00%")
+c = 0
+for id in photos:
+    photo = data.getPhotoFromId(id)
+    photo.saveCopy(dirs[0])
+    photo.makePreview(dirs[1], 640)
+    photo.makeThumbnail(dirs[2])
+    photoPageName = makePhotoPage(photo, curPage.fileName)
+    curPage.addCode("<a href=\"%s\"><img src=\"%s\" class=\"thumb\"/></a>" %
+        (photoPageName, photo.thumbPath))
+    c += 1
+    if c % picsPerPage == 0 and c < len(photos):
+        curPage.addCode("</div>")
+        curPage.writePage()
+        pageCounter += 1
+        curPage = WebPage("page%2d" % pageCounter, albumName)
+        curPage.addCode("<div class=\"square\">")
+    echo("\b\b\b%2d%%" % ((100 * c) / len(photos)))
+echo("\b\b\b\b\tDONE\n")
+curPage.addCode("</div>")
+curPage.writePage()
