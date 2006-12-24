52f5d3682571d43c3f434fa35eae29191867565a
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2006-12-24 05:09:11 +0000

    create 0.0.2 branch

--- setup.py
+++ setup.py
@@ -25,9 +25,21 @@ def main():
         long_description = '''In this release
         ===============
     
-        Fixed in 0.0.2:
+        New in 0.0.2:
         
-        A lot :)
+        - better navigation, makepage output looks almost like scry now
+	- works on Linux with Panther iPhoto 2 and Tiger
+	- cleaner help and option parsing thanks to optparse
+	- xml file content is cached with cPickle and cache is used when the file hasn't changed
+	- lots of bug fixes
+	- regular python package (some problem, see first issue though, transition is not finished)
+
+	Known issues:
+	- You may have to pytof from the source script directory, regulare package install 
+	  with python setup.py install wasn't tested. (if you can give it a try and report problems it's great
+	  there might be problem finding the resources (.css files) then
+	- Versionning is broken (issue 11)
+	- Canceled operations should cleanup target dir (issue 13)
         
         ''',
         author = "Mathieu Robin",
