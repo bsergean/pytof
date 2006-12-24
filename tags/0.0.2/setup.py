cc77f895c907c7a48c2613e7a1aaa8044b396351
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2006-12-24 05:10:02 +0000

    create 0.0.2 tag from 0.0.2 branch

---
+++ tags/0.0.2/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# -*- python -*-
+#
+#*****************************************************************************
+#
+# See LICENSE file for licensing and to see where does some code come from
+#
+#*****************************************************************************
+#
+# build package
+#
+
+from distutils.core import setup
+
+def main():
+
+    # data files
+    # http://docs.python.org/dist/node13.html
+
+    setup(
+        name = "pytof",
+        version = '0.0.2',
+        description = "Exports album from iPhoto Libraries",
+        long_description = '''In this release
+        ===============
+    
+        New in 0.0.2:
+        
+        - better navigation, makepage output looks almost like scry now
+	- works on Linux with Panther iPhoto 2 and Tiger
+	- cleaner help and option parsing thanks to optparse
+	- xml file content is cached with cPickle and cache is used when the file hasn't changed
+	- lots of bug fixes
+	- regular python package (some problem, see first issue though, transition is not finished)
+
+	Known issues:
+	- You may have to pytof from the source script directory, regulare package install 
+	  with python setup.py install wasn't tested. (if you can give it a try and report problems it's great
+	  there might be problem finding the resources (.css files) then
+	- Versionning is broken (issue 11)
+	- Canceled operations should cleanup target dir (issue 13)
+        
+        ''',
+        author = "Mathieu Robin",
+        author_email = "mathieu.robin@gmail.com",
+        maintainer = 'Benjamin Sergeant',
+        maintainer_email = 'bsergean@gmail.com',
+        url = 'http://code.google.com/p/pytof/',
+        cmdclass = {},
+        packages = ['pytof'],
+        classifiers = [],
+        scripts = ['scripts/pytof.py'],
+        data_files = [ ('share', ['share/scry.css']) ]
+        )
+
+if __name__ == '__main__':
+    main()
