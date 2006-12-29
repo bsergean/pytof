70928324b9326a235cc7e20e01387b9ae284aa83
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2006-12-29 23:50:03 +0000

    add some ftp unit tests

--- test/ftpTest.py
+++ test/ftpTest.py
@@ -14,79 +14,94 @@ __author__ = 'Benjamin Sergeant'
 import sys
 sys.path.insert(1, '../pytof')
 
+import unittest
+
 import os
 from ftp import ftpUploader
 from os.path import join, basename
+from os import listdir, lstat, remove, chdir, walk, mkdir
+from stat import S_ISDIR
 from utils import GetTmpDir, maybemakedirs
-
-import stat
-# stolen and adapted from shutils rmtree
-def my_rmtree(path):
-    try:
-        names = os.listdir(path)
-    except:
-        pass
-    for name in names:
-        fullname = os.path.join(path, name)
-        try:
-            mode = os.lstat(fullname).st_mode
-        except: pass
-        if stat.S_ISDIR(mode):
-            rmtree(fullname)
-        else:
-            try:
-                os.remove(fullname)
-            except: pass
-    try:
-        os.rmdir(path)
-    except: pass
+from shutil import copy, rmtree
+from tempfile import mkdtemp, mktemp
 
 def create(file, content):
     fd = open(file, 'w')
     fd.write(content)
     fd.close()
 
+def diff(a, b):
+    ''' Call the real diff function on two Unix files or directories '''
+    exitCode = os.system('diff %s %s' % (a, b))
+    return exitCode == 0
+
+class FTP_TC(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = mkdtemp()
+
+        passwd = os.environ.get('PASSWD', '')
+        if not passwd:
+            print 'no password in $PASSWD'
+            sys.exit(1)
+        else:
+            print 'start ftp connection'
+        self.ftp = ftpUploader('localhost', 'bsergean', passwd)
+
+    def tearDown(self):
+        rmtree(self.tempdir)
+
+    def testUpLoadOneFile(self):
+        tmp = join(self.tempdir, 'b')
+        create(tmp, 'youcoulele')
+
+        remoteTmp = mktemp()
+        self.ftp.upload(tmp, remoteTmp)
+        self.assert_ (diff (tmp, remoteTmp))
+
+    def testMirrorDir(self):
+        newDir = 'mirror_dir'
+        topDir = join(self.tempdir, newDir)
+        maybemakedirs(topDir)
+        
+        tmpftpfile = 'a_file'
+        create(join(self.tempdir, tmpftpfile), 'youcoulele')
+        tmpftpfile = 'another_file'
+        create(join(self.tempdir, tmpftpfile), 'warzazat')
+
+        nestedDir = join(topDir, 'a_dir')
+        mkdir(nestedDir)
+        
+        tmpftpfile = 'a_file'
+        create(join(nestedDir, tmpftpfile), 'youcoulele')
+        tmpftpfile = 'another_file'
+        create(join(nestedDir, tmpftpfile), 'warzazat')
+        
+        self.ftp.cp_rf(topDir, self.tempdir)
+        self.assert_ (diff (topDir, join(self.tempdir, newDir)))
+
+        
 if __name__ == "__main__":
+    unittest.main()
+
+    sys.exit(0)
     
     ###########
-    # ftp tests
-    # upload a file
-    tmpDir = GetTmpDir()
-    
-    tmpftpfile = 'b'
-    create(tmpftpfile, 'youcoulele')
+    # copy -r one dir to another
+    print 'mirror'
+    ftp.mkd('/home/bsergean/tmp/Capture3D-8.0.0')
+    ftp.mirror_r('/usr/local/Adobe/Capture3D-8.0.0', '/home/bsergean/tmp/Capture3D-8.0.0/')
+    ftp.rmtree('/home/bsergean/tmp/Capture3D-8.0.0')
 
-    # this test needs the ftp password to be stored
-    # in the PASSWD env variable
-    passwd = os.environ.get('PASSWD', '')
-    if not passwd:
-        print 'no password in $PASSWD'
-        sys.exit(1)
-    else:
-        print 'start ftp connection'
-    
-    ftp = ftpUploader('localhost', 'bsergean', passwd)
-    ftp.infos()
-    for f in ftp.ls():
-        print f
+    ftp.cp_rf('/usr/local/Adobe/Capture3D-8.0.0', '/home/bsergean/tmp')
+
+    sys.exit(0)
 
     ###########
     # ftp tests
     # upload a directory
-    topDir = join(tmpDir, 'mirror_dir')
-    maybemakedirs(topDir)
-    
-    tmpftpfile = 'a_file'
-    create(join(topDir, tmpftpfile), 'youcoulele')
-    tmpftpfile = 'another_file'
-    create(join(topDir, tmpftpfile), 'warzazat')
 
-    nestedDir = join(topDir, 'a_dir')
-    maybemakedirs(nestedDir)
-
-    tmpftpfile = 'a_file'
-    create(join(nestedDir, tmpftpfile), 'youcoulele')
-    tmpftpfile = 'another_file'
-    create(join(nestedDir, tmpftpfile), 'warzazat')
-
-    ftp.mirror(topDir)
+    ###########
+    # ftp tests
+    # delete a dir
+    ftp.rmtree(topDir)
