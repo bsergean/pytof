2d396e1bcbe280111e148c14477d5168fa5e9283
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2007-01-14 07:45:59 +0000

    move tar code in a single function in utils and added a test case

--- test/utils_test.py
+++ test/utils_test.py
@@ -12,10 +12,12 @@ __revision__ = '$Id$  (C) 2007 GPL'
 
 
 from unittest import TestCase
-from utils import RemoveSpecificChars, UnixFind, urlExtractor
-from os.path import join
+from utils import RemoveSpecificChars, UnixFind, urlExtractor, mktar, maybemakedirs
+from os.path import join, getsize
 from tempfile import mkdtemp, mktemp
-from shutil import rmtree
+from shutil import rmtree, copytree
+from os import mkdir
+import tarfile
 
 class TestUtils(TestCase):
 
@@ -23,8 +25,8 @@ class TestUtils(TestCase):
         self.tempdir = mkdtemp()
         self.baseDir = join('data', 'galleries')
 
-    def tearDown(self):
-        rmtree(self.tempdir)
+#    def tearDown(self):
+#        rmtree(self.tempdir)
     
     def testRemoveSpecificChars(self):
         testCases = [
@@ -53,3 +55,67 @@ class TestUtils(TestCase):
 
         for url in urlExtractor(join(leschats, 'index.html')):
             self.assertEquals(url, 'http://code.google.com/p/pytof/')
+
+    def create_dummy_dir(self, newDir):
+        '''
+        This one is duplicated from ftp_utils: We should create a class
+        that inherit unittest.TestCase, with this method in it in test.py.
+        We should take care of the self.tmpdir also.
+        '''
+
+        def create(file, content):
+            fd = open(file, 'w')
+            fd.write(content)
+            fd.close()
+        
+        topDir = join(self.tempdir, newDir)
+        maybemakedirs(topDir)
+
+        # first level
+        tmpftpfile = 'a_file'
+        create(join(self.tempdir, tmpftpfile), 'youcoulele')
+        tmpftpfile = 'another_file'
+        create(join(self.tempdir, tmpftpfile), 'warzazat')
+
+        # second level
+        tmpftpfile = 'one_file'
+        create(join(topDir, tmpftpfile), 'youcoulele')
+
+        # third level
+        nestedDir = join(topDir, 'a_dir')
+        mkdir(nestedDir)
+
+        tmpftpfile = 'a_file'
+        create(join(nestedDir, tmpftpfile), 'youcoulele')
+        tmpftpfile = 'another_file'
+        create(join(nestedDir, tmpftpfile), 'warzazat')
+
+        return topDir
+    
+    def testTar(self):
+        '''
+        Create an archve from a temp dir created with create_dummy_dir
+        put one file at the prefix level, tar the archive
+        extract the archive, and check that the file is there and
+        that its size is the same as the original one.
+        '''
+        
+        dummyDir = self.create_dummy_dir('tar_test_dir')
+        tmpftpfile = 'one_file'
+        topLevelFile = join(dummyDir, tmpftpfile)
+        topLevelFileSize = getsize(topLevelFile)
+        
+        tarFile = join(self.tempdir, 'foo.bar')
+        mktar(tarFile, dummyDir, 'a_dir', [topLevelFile])
+
+        # now list the file within the archive and look for topLevelFile
+        tar = tarfile.open(tarFile)
+        found = False
+        for tarinfo in tar:
+            if tarinfo.name == tmpftpfile:
+                found = True
+                size = tarinfo.size
+                break
+
+        self.assert_(found)
+        self.assertEquals(size, topLevelFileSize)
