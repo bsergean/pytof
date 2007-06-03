c66f0646255a5cd7214170649cf2160caa7dac54
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2007-06-03 20:33:48 +0000

    prepare to push package on pypi

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: iso-8859-1 -*-
-# -*- python -*-
-#
-#*****************************************************************************
-#
-# See LICENSE file for licensing and to see where does some code come from
-#
-#*****************************************************************************
-#
-# build package
-#
+"""pytof: Share your pictures.
 
-from distutils.core import setup
-import sys
-from pytof.version import __version__
-
-pytof_long_description = '''
+pytof helps you share your iPhoto pictures, and any pictures that are stored in a flat directory since version 0.3.0. It works on Mac, Windows and Linux.
 In this release
         ===============
+        New in 0.8.2:
+        - Put package on pypi.
+        - Unit test works again
+        - Do not chocke with movies
+ 
        New in 0.7.0:
        - Windows installer
        - Primitive user interface based on pygtk.
@@ -51,26 +42,50 @@ In this release
 	  there might be problem finding the resources (.css files) then
 	- Versionning is broken (issue 11)
 	- Canceled operations should cleanup target dir (issue 13)
-'''
+"""
+
+classifiers = """\
+Development Status :: 4 - Beta
+Classifier: Environment :: Console (Text Based)
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Topic :: Multimedia
+Operating System :: Microsoft
+Operating System :: MacOS
+Operating System :: Unix
+"""
+
+# Copyright (C) 2006, 2007 GPL
+# Written by Benjamin Sergeant <bsergean@gmail.com>
+
+__revision__ = '$Id: ctof.py 308 2007-03-16 04:29:16Z bsergean $  (C) 2007 GPL'
+
+from distutils.core import setup
+import sys
+from pytof.version import __version__
+
+doclines = __doc__.split("\n")
 
 def main():
 
     # data files
     # http://docs.python.org/dist/node13.html
     # http://www.thescripts.com/forum/thread164172.html
+    # http://www.python.org/~jeremy/weblog/030924.html
 
     setup(
         name = "pytof",
         version = __version__,
-        description = "Exports album from iPhoto Libraries",
-        long_description = pytof_long_description,
+        description = doclines[0],
         author = "Mathieu Robin",
         author_email = "mathieu.robin@gmail.com",
         maintainer = 'Benjamin Sergeant',
         maintainer_email = 'bsergean@gmail.com',
         url = 'http://code.google.com/p/pytof/',
         cmdclass = {},
-        classifiers = [],
+        classifiers = filter(None, classifiers.split("\n")),
+        long_description = "\n".join(doclines[2:]),
         scripts = ['ctof.py', 'gtof/gtof.py'],
         packages = ['pytof'],
 #        package_dir = {'pytof': 'pytof'},
