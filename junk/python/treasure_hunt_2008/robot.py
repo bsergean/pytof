4b58dc5fc4ca7a9f5bea06c21a803955e19c705c
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2008-06-08 17:41:22 +0000

    4th first treasure solved

--- junk/python/treasure_hunt_2008/robot.py
+++ junk/python/treasure_hunt_2008/robot.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 
-class Robot:
-    def __init__(self, N, P):
-        self.N = N
-        self.P = P
+cache = {}
+def set_cache_result(n, p, res):
+    key = '%d,%d' % (n, p)
+    cache[key] = res
 
-    def compute(self, n, p):
+def is_in_cache(n, p):
+    key = '%d,%d' % (n, p)
+    return key in cache
 
-        if n <= 1 and p <= 1: return 1
+def get_cache_result(n, p):
+    key = '%d,%d' % (n, p)
+    return cache[key]
+
+def robot(n, p):
+
+    if n == 1 or p == 1: return 1
+
+    if is_in_cache(n,p):
+        return get_cache_result(n,p)
+
+    res = 0
+    for j in xrange(1, p+1):
+        res += robot(n-1, j)
+
+    return res
+
+def populate_cache(n, p):
     
-        for j in xrange(P):
-            res += Robot(n-1,j)
+    for i in xrange(1, n+1):
+        for j in xrange(1, p+1):
+            res = robot(i,j)
+            print i,j,res
+            set_cache_result(i,j,res)
 
-        return res
-
-r = Robot(7,3)
-print r.compute()
+import sys
+if len(sys.argv) == 3:
+    n, p = sys.argv[1:]
+    print populate_cache(int(n), int(p))
+else:
+    print populate_cache(47, 43)
