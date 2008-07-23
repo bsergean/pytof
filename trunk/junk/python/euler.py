56b395a8d5edaf84987628e7322d1599f657c394
sandrine.ribeau <sandrine.ribeau@e2413325-ff24-0410-b344-2fef527c98a9>
2008-07-23 06:18:37 +0000

    euler

--- trunk/junk/python/euler.py
+++ trunk/junk/python/euler.py
@@ -465,7 +465,25 @@ def level13():
     S = sum([int(l) for l in datas.splitlines()])
     print str(S)[0:10]
 
-def level14(): pass
+def level14():
+
+    def collatz_sequence(seed):
+        n = seed
+        i = 1
+        while n != 1:
+            if n % 2 == 0:
+                n /= 2
+            else:
+                n = 3*n + 1
+            i += 1
+        return i
+
+    max = -1
+    for i in xrange(1,1e6+1):
+        res = collatz_sequence(i)
+        if res > max:
+            max = res
+            print i, max
 
 def level15():
     ''' The same problem as in google treasure hunt (which was 
@@ -577,6 +595,25 @@ def level17():
         S += L
     print S
 
+def level18():
+    triangle = '''
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+'''
+
 def level20():
     def fact(n):
         if n == 1: return 1
@@ -609,4 +646,14 @@ def level25():
             print i
             break
 
-level15()
+def level26():
+    '''
+Best precision for a remainder of a float ?
+>>> print "%.100g" % (1 /7.0)
+0.142857142857142849212692681248881854116916656494140625
+
+What about using gmpy / mpq type (from an email thread)
+    ''' 
+    pass
+
+level14()
