43b0c5b7242d917c61d842cfad32b4c3eb677fcf
sandrine.ribeau <sandrine.ribeau@e2413325-ff24-0410-b344-2fef527c98a9>
2008-07-30 16:10:53 +0000

    euler

--- junk/python/euler.py
+++ junk/python/euler.py
@@ -700,7 +700,7 @@ def level23():
     abundant = {}
     for i in xrange(10, 28123 + 1):
         s = D.proper_divisor_sum(i)
-        if s >= i:
+        if s > i:
             abundant[i] = s
             print i,s
 
@@ -710,7 +710,7 @@ def level23():
                 return True
         return False
 
-    print sum(i for i in xrange(10, 28123 + 1) if abundant_sumable(i))
+    print sum(i for i in xrange(10, 28123 + 1) if not abundant_sumable(i))
 
 
 def permutations(L):
