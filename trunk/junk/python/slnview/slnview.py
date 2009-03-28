96d287ac0906e85ea4b4cdf05ba6279784b8a58d
sandrine.ribeau <sandrine.ribeau@e2413325-ff24-0410-b344-2fef527c98a9>
2009-03-28 00:12:18 +0000

    slnview: building the AST works with simple_complete

--- trunk/junk/python/slnview/slnview.py
+++ trunk/junk/python/slnview/slnview.py
@@ -8,6 +8,7 @@ def sln_input():
     input = 'global'
     input = 'nested'
     input = 'platforms'
+    input = 'a3d'
     input = 'simple_complete'
     return open(input + '.sln').read()
         
@@ -88,94 +89,145 @@ if do_lex_only:
     import sys ; sys.exit(0)
 
 # YACC
+# Node class not used for now.
+import types # types.ListType
+class Node:
+    def __init__(self,type,children=None,leaf=None):
+         self.type = type
+         if children:
+              self.children = children
+         else:
+              self.children = [ ]
+         self.leaf = leaf
+
+    def rec_print(self, t, spaces = ''):
+        print spaces, t.type, t.leaf
+        for b in t.children:
+            self.rec_print(b, spaces + ' ')
+
+    def do_print(self):
+        self.rec_print(self)
 
 # TODO: project_section is optional
 def p_main_file(p):
     'main_file : header main_statement_group'
+    global the_ast
+    the_ast = [p[1], p[2]]
 
 # Cannot Use lex to take the long string below as a single lex token
 # Microsoft Visual Studio Solution File, Format Version 9.00
 # 1         2      3      4        5   , 6      7
 def p_header(p):
     'header : NAME NAME NAME NAME NAME COMMA NAME NAME FLOAT'
+    p[0] = p[9]
 
 def p_main_statement_group(p):
     '''main_statement_group : main_statement main_statement_group
                             | main_statement'''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
 
 def p_main_statement(p):
     '''main_statement : PROJECT project_statement
                       | GLOBAL  global_statement'''
+    p[0] = p[2]
 
 def p_project_statement(p):
-    '''project_statement : LPAREN quoted RPAREN project_3_uple project_section ENDPROJECT
-                         | LPAREN quoted RPAREN project_3_uple ENDPROJECT'''
+    '''project_statement : project_declaration project_section ENDPROJECT
+                         | project_declaration ENDPROJECT'''
+    if len(p) == 4:
+        p[0] = [p[1], p[2]]
+    else:
+        p[0] = [p[1]]
+
+def p_project_declaration(p):
+    'project_declaration : LPAREN quoted RPAREN project_3_uple'
+    p[0] = [p[2], p[4]]
 
 def p_project_section(p):
     'project_section : PROJECTSECTION LPAREN PROJECTDEPENDENCIES RPAREN project_section_body ENDPROJECTSECTION'
+    p[0] = p[5]
 
 def p_project_section_body(p):
     'project_section_body : EQUALS POSTPROJECT post_project_list'
+    p[0] = p[3]
 
 def p_post_project_list(p):
-    '''post_project_list : GUID EQUALS GUID post_project_list
-                         | GUID EQUALS GUID '''
-    print p[1], p[3]
+    '''post_project_list : post_project_core post_project_list
+                         | post_project_core '''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
+
+def p_post_project_core(p):
+    'post_project_core : GUID EQUALS GUID'
+    p[0] = [p[1], p[3]]
     
 def p_project_3_uple(p):
     'project_3_uple : EQUALS quoted COMMA quoted COMMA quoted'
+    p[0] = [p[2], p[4], p[6]]
 
 def p_quoted(p):
     'quoted : DOUBLEQUOTE expr DOUBLEQUOTE'
+    p[0] = p[2]
 
 def p_expr(p):
     '''expr : GUID
             | name_list
             | VCPROJPATH'''
-    print 'Expr', p[1]
+    p[0] = p[1]
 
 def p_name_list(p):
     '''name_list : NAME name_list
                  | NAME'''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
 
 ###
 #  GLOBAL
 ### 
 def p_global_statement(p):
     'global_statement : globalsection_statement_group ENDGLOBAL'
+    p[0] = p[1]
 
 def p_global_statement_group(p):
     '''globalsection_statement_group : GLOBALSECTION globalsection_statement ENDGLOBALSECTION globalsection_statement_group
                                      | GLOBALSECTION globalsection_statement ENDGLOBALSECTION '''
+    p[0] = [p[2]] if len(p) == 4 else p[2] + p[4]
 
 def p_globalsection_statement(p):
     '''globalsection_statement : LPAREN SOLUTIONCONFIGURATIONPLATFORMS RPAREN solution_conf_body
                                | LPAREN PROJECTCONFIGURATIONPLATFORMS RPAREN project_conf_body
                                | LPAREN SOLUTIONPROPERTIES RPAREN properties_body
                                | LPAREN NESTEDPROJECTS RPAREN nested_body'''
+    p[0] = p[4] # FIXME
 
 def p_solution_conf_body(p):
     'solution_conf_body : EQUALS PRESOLUTION solution_conf_list'
+    p[0] = p[3]
 
 def p_pre_solution_list(p):
-    '''solution_conf_list : CONF arch EQUALS CONF arch solution_conf_list
-                          | CONF arch EQUALS CONF arch '''
-    print p[1]
-    return
-    for i,e in enumerate(p):
-        print 'pre_solution_list', i, e
+    '''solution_conf_list : pre_solution_core solution_conf_list
+                          | pre_solution_core '''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
+
+def p_pre_solution_core(p):
+    'pre_solution_core : CONF arch EQUALS CONF arch'
+    p[0] = [p[1], p[2], p[4], p[5]]
 
 def p_arch(p):
     '''arch : WIN32
             | X64'''
+    p[0] = p[1]
 
 def p_project_conf_body(p):
     'project_conf_body : EQUALS POSTSOLUTION project_conf_list'
+    p[0] = p[3]
 
 def p_project_conf_list(p):
-    '''project_conf_list : GUID DOT CONF arch DOT build_conf EQUALS CONF arch project_conf_list
-                         | GUID DOT CONF arch DOT build_conf EQUALS CONF arch '''
-    print p[1], p[3], p[7]
+    '''project_conf_list : project_conf_core project_conf_list
+                         | project_conf_core'''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
+
+def p_project_conf_core(p):
+    'project_conf_core : GUID DOT CONF arch DOT build_conf EQUALS CONF arch'
+    p[0] = [p[1], p[3], p[7], p[5]] # FIXME
     return
     for i,e in enumerate(p):
         print 'pre_solution_list', i, e
@@ -183,21 +235,33 @@ def p_project_conf_list(p):
 def p_build_conf(p):
     '''build_conf : ACTIVECONFIG
                   | BUILD0'''
+    p[0] = p[1]
 
 def p_properties_body(p):
     'properties_body : EQUALS PRESOLUTION properties_list'
+    p[0] = p[3]
 
 def p_properties_list(p):
-    '''properties_list : NAME EQUALS NAME properties_list
-                       | NAME EQUALS NAME'''
+    '''properties_list : properties_core properties_list
+                       | properties_core'''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
+
+def p_properties_core(p):
+    'properties_core : NAME EQUALS NAME'
+    p[0] = [p[1], p[3]]
 
 def p_nested_body(p):
     'nested_body : EQUALS PRESOLUTION nested_body_list'
+    p[0] = p[3]
 
 def p_nested_body_list(p):
-    '''nested_body_list : GUID EQUALS GUID nested_body_list
-                        | GUID EQUALS GUID '''
-    print p[1], p[3]
+    '''nested_body_list : nested_body_core nested_body_list
+                        | nested_body_core '''
+    p[0] = [p[1]] if len(p) == 2 else p[1] + p[2]
+
+def p_nested_body_core(p):
+    'nested_body_core : GUID EQUALS GUID'
+    p[0] = [p[1], p[3]]
 
 def p_error(p):
     print "Syntax error at '%s'" % p.value
@@ -206,4 +270,5 @@ import ply.yacc as yacc
 yacc.yacc()
 
 yacc.parse(sometext)
+print the_ast
 
