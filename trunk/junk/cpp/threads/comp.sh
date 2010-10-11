e85242721f52afe1a8351114db394491b7502333
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2010-10-11 16:07:59 +0000

    spolsky C questions

--- trunk/junk/cpp/threads/comp.sh
+++ trunk/junk/cpp/threads/comp.sh
@@ -1,1 +1,1 @@
-gcc -O3 pthread_wc.c -lpthread
+gcc -o wc_l -O3 pthread_wc.c -lpthread
---
+++ trunk/junk/cpp/tutorial/reverse.c
@@ -0,0 +1,36 @@
+/* vim:set tabstop=4 shiftwidth=4 expandtab: */
+#include <stdio.h>
+#include <strings.h>
+
+void reverse(char s[]) {
+    int i, j;
+    puts(s);
+    
+    for (i = 0, j = strlen(s) - 1; 
+         i < j; 
+         ++i, --j) {
+         #if 0
+        printf("s[i] = %c\n", s[i]);
+        printf("s[j] = %c\n", s[j]);
+        puts("");
+        continue;
+        #endif
+        
+        int tmp = s[j];
+        s[j] = s[i];
+        s[i] = tmp;
+    }
+    puts(s);
+}
+
+int main() {
+    char foo[] = "0123456789";
+    char bar[] = "0123456789A";
+    char baz[]  = "0";
+    char bam[]  = "";
+
+    reverse(foo);
+    reverse(bar);
+    reverse(baz);
+    reverse(bam);
+}
