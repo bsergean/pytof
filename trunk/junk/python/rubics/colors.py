9b5640e1e61a7bf815b3f46105808da1298e5511
bsergean <bsergean@e2413325-ff24-0410-b344-2fef527c98a9>
2011-03-30 06:01:37 +0000

    rubics wip

---
+++ trunk/junk/python/rubics/colors.py
@@ -0,0 +1,21 @@
+# http://en.wikipedia.org/wiki/Web_colors
+black = [0, 0, 0]
+white = [0xFF, 0xFF, 0xFF]
+
+red =   [0xFF, 0, 0]
+green = [0x22, 0x8B, 0]
+blue =  [0x46, 0x82, 0xB4]
+
+yellow = [0xFF, 0xD7, 0]
+orange = [0xFF, 0x45, 0]
+
+if False: # Set to True to test cube_ops.py
+    black = 'black'
+    white = 'white'
+
+    red =   'red'
+    green = 'green'
+    blue =  'blue'
+
+    yellow = 'yellow'
+    orange = 'orange'
