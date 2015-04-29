3a4cb0f872e8a2d6c8a27f0386393b017520e524
Benjamin Sergeant <bsergean@gmail.com>
2015-04-28 18:50:35 -0700

    More comments for stacks_hanoi.cc

--- junk/rand/stacks_hanoi.cc
+++ junk/rand/stacks_hanoi.cc
@@ -29,6 +29,10 @@ std::stack<int> s1;
 std::stack<int> s2;
 std::stack<int> s3;
 
+//
+// Push values on the stack. Large block are 
+// represented by large entries.
+//
 void
 init(int N)
 {
@@ -37,6 +41,9 @@ init(int N)
     }
 }
 
+//
+// Apply an operation on our 3 stacks
+//
 void
 move(Op op)
 {
@@ -53,6 +60,9 @@ move(Op op)
     }
 }
 
+//
+// Execute a batch of operations
+//
 void
 execute(const Ops& ops)
 {
@@ -72,6 +82,10 @@ extend(Ops& ops, const Ops& copy)
     }
 }
 
+//
+// Reverse a list of operations,
+// then flip the operations themselves.
+//
 void
 reverse(Ops& ops)
 {
@@ -82,6 +96,19 @@ reverse(Ops& ops)
     std::reverse(ops.begin(), ops.end());
 }
 
+//
+// The main recursive solver.
+// Strategy:
+//
+// 1. Solve the problem for (N-1).
+// 2. Move the last element (large disk) to the second
+//    empty stack.
+// 3. "Un-solve the problem for (N-1), that is move all disk
+//    which were on the right-end side / stack 3 back to stack 1.
+// 4. Move the large disk to stack 3 which is now clear.
+// 5. Move the first stack content to the stack 3, using 
+//    the operations from solving at N-1.
+//
 void
 solve(int N, Ops& ops)
 {
@@ -92,22 +119,25 @@ solve(int N, Ops& ops)
         return;
     }
 
+    // 1.
     solve(N-1, ops);
 
     Ops copy(ops); // ops it takes to solve for N-1 
 
-    // Move large one to the middle position
+    // 2. Move large one to the middle position
     move(Op(1, 2));
     ops.push_back(Op(1, 2)); // record that move
 
+    // 3. "un-solve", by reversing the moves.
     reverse(copy);
     execute(copy);
     extend(ops, copy);
 
-    // Move large one to the final position
+    // 4. Move large one to the final position
     move(Op(2, 3));
     ops.push_back(Op(2, 3)); // record that move
 
+    // 5. reverse our op list, then execute it.
     reverse(copy);
     execute(copy);
     extend(ops, copy);
